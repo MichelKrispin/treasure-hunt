8cdb50c6f11d857fcb631c740a2e27addcb3e27f
Michel Krispin <michel.krispin@gmail.com>
2021-05-14 19:13:20 +0200

    Fixed upload bugs

--- treasurehunt/settings.py
+++ treasurehunt/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)fql#-v)45e$5v%yg*aur)o6e2@wap+j#r=yyuj=*rp^uh$)8r'
+try:
+    SECRET_KEY = os.environ['SECRET_KEY']
+except KeyError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+try:
+    DEBUG = True if os.environ['DEBUG_MODE'] else False
+except KeyError:
+    pass
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +134,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
