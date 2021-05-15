cefd576d648f0f2a37fe37a3d30beaca2231484f
Michel Krispin <michel.krispin@gmail.com>
2021-05-15 11:25:25 +0200

    Improved amin site

--- hunting/admin.py
+++ hunting/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.db.models.functions import Lower
 
 from .models import Map, Stage
 
-admin.site.register(Map)
-admin.site.register(Stage)
+@admin.register(Stage)
+class StageAdmin(admin.ModelAdmin):
+    list_display = ('question', 'level', 'corresponding_map',) 
+    
+    def get_ordering(self, request):
+        return [Lower('corresponding_map'), Lower('level')]
+
+@admin.register(Map)
+class MapAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'password',) 
+    date_hierarchy = 'date'    
+    
+    def get_ordering(self, request):
+        return [Lower('date')]
