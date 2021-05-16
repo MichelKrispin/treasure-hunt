2e670b9c999b904da50fb1231048554b294347d8
MichelKrispin <michel.krispin@gmail.com>
2021-05-16 18:01:40 +0200

    Added fuzzy answer difference

--- hunting/views.py
+++ hunting/views.py
@@ -1,4 +1,5 @@
 import random
+from fuzzywuzzy import fuzz
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
@@ -8,25 +9,35 @@ from django.views import generic
 from .models import Map, Stage
 
 # One of these are randomly selected
-errors = [
-    'Nicht ganz',
-    'Nicht wirklich',
-    'Versuch es doch einmal nochmal',
-    'Näää',
-    'Knapp. Glaub ich.',
-    'Nicht mal nah dran',
-    'So kurz davor',
-    'Achso? Das war ja nicht so wirklich richtig...',
-    'Uff...',
-    'Äh nein?',
-    'Was war denn das für eine schräge Idee?',
-    'Ganz sicher nicht...',
-    'Pffffffff.',
-    'Nö',
-    'Einfach nur falsch.',
-    'Leider nicht',
-    'Hehe. Ne.',
-]
+errors = {
+    'close': [
+        'Nicht ganz',
+        'Knapp. Glaub ich.',
+        'So kurz davor',
+        'Uff...',
+        'Ganz knappes Ding',
+        'Wirkling eng',
+        'Naja fast',
+
+    ],
+    'far': [
+        'Nicht wirklich',
+        'Näää',
+        'Nicht mal nah dran',
+        'Einfach nur falsch.',
+        'Pffffffff sicher nicht.',
+        'Was war denn das für eine schräge Idee?',
+        'Ganz sicher nicht...',
+    ],
+    'always': [
+        'Versuch es doch einmal nochmal',
+        'Achso? Das war ja nicht so wirklich richtig...',
+        'Nö',
+        'Leider nicht',
+        'Hehe. Ne.',
+        'Äh nein?',
+    ]
+}
 
 
 class IndexView(generic.ListView):
@@ -49,49 +60,35 @@ def check_answer(correct_answer, given_answer):
     correct_answer = correct_answer.lower()
     given_answer = given_answer.lower()
 
-    # If the length of the string is only one (one word or one number)
-    # just compare them
-    space_splitted_correct_answer = correct_answer.split(' ')
-    dot_splitted_correct_answer = correct_answer.split('.')
-    if (len(space_splitted_correct_answer) == 1 and 
-        len(dot_splitted_correct_answer) == 1 ):
-        return correct_answer == given_answer
+    # Now get the difference
+    try:
+        answer_ratio = fuzz.token_sort_ratio(correct_answer, given_answer)
+    except Exception as e:
+        print(f'Error with fuzzywuzzy ({correct_answer}, {given_answer})\n{e}')
 
-    # Otherwise if there are multiple words check whether each
-    # is also contained in the given answer
-    len_space_splitted_correct_answer = len(space_splitted_correct_answer)
-    if len_space_splitted_correct_answer > 1:
-        num_correct = 0
-        for parts in space_splitted_correct_answer:
-            if '.' in parts: # If dot inside remove
-                parts = parts.replace('.', '')
-            if parts in given_answer:
-                num_correct += 1
-        # If there are as many correct elements as elements in the answer its correct
-        return num_correct == len_space_splitted_correct_answer
+    # Then give a correct answer depending on the correctness
+    if answer_ratio > 95: # Above 95 is correct
+        return 'correct'
 
-    # If there are no spaces but dots it is probably a date
-    len_dot_splitted_correct_answer = len(dot_splitted_correct_answer)
-    if len_dot_splitted_correct_answer > 1:
-        num_correct = 0
-        for parts in dot_splitted_correct_answer:
-            if parts in given_answer:
-                num_correct += 1
-        # If there are as many correct elements as elements in the answer its correct
-        return num_correct == len_dot_splitted_correct_answer
+    # 50 Percent Chance to get a always possible answer
+    if random.choice([True, False]):
+        return 'always'
 
-    # If nothing happend? No idea
-    return False
+    if answer_ratio > 60: # Quite close
+        return 'close'
+
+    return 'far' # Otherweise its far away
 
 def check_stage(request, id):
     current_stage = get_object_or_404(Stage, pk=id)
-    
+
     # This here is the answer checking thing
-    if check_answer(current_stage.answer, request.POST['answer']):
+    answer_type = check_answer(current_stage.answer, request.POST['answer'])
+    if answer_type == 'correct':
         # If the answer is correct get the stage with the next higher level
         next_stage = Stage.objects.filter(level__gt=current_stage.level).first()
         return HttpResponseRedirect(reverse('stage', args=(next_stage.slug,)))
-    
+
     # If the answer was wrong just return a random error and the same stage again
-    return render(request, 'hunting/stage.html', { 'stage': current_stage, 'error': random.choice(errors) })
+    return render(request, 'hunting/stage.html', { 'stage': current_stage, 'error': random.choice(errors[answer_type]) })
 
